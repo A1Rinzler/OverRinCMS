31c38e250ff9f2e5dfe4c57456307ea2199cd3c0
Dmitrii Frolov <tryfrod@gmail.com>
2026-08-07 14:44:33 +0300

    init

---
+++ src/main/java/ru/dmitrii/OverRinCMS/OverRinCmsApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitrii.OverRinCMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverRinCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverRinCmsApplication.class, args);
+	}
+
+}
