776dd23cd164beec6f201dd326023bf4ca67ba6b
Dmitrii Frolov <tryfrod@gmail.com>
2026-08-24 17:49:06 +0300

    геттеры сеттеры для person, забыл

--- src/main/java/ru/dmitrii/OverRinCMS/model/Person.java
+++ src/main/java/ru/dmitrii/OverRinCMS/model/Person.java
@@ -43,4 +43,59 @@ public class Person {
     @Column(name = "update_time")
     private LocalDateTime update_time;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(LocalDateTime create_time) {
+        this.create_time = create_time;
+    }
+
+    public LocalDateTime getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(LocalDateTime update_time) {
+        this.update_time = update_time;
+    }
 }
