5e52ee752bba38ddc6cadce3fe605024b9ccf61f
Dmitrii Frolov <tryfrod@gmail.com>
2026-08-26 17:40:36 +0300

    юзер контроллер добавление пользователя для сайта. в основном будет для админ персонала.

---
+++ src/main/java/ru/dmitrii/OverRinCMS/controller/UserController.java
@@ -0,0 +1,29 @@
+package ru.dmitrii.OverRinCMS.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.dmitrii.OverRinCMS.dto.PersonDTO;
+import ru.dmitrii.OverRinCMS.service.PersonService;
+
+@RestController
+@RequestMapping("/adduser")
+public class UserController {
+
+    private final PersonService personService;
+
+    @Autowired
+    public UserController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping("")
+    public ResponseEntity<HttpStatus> addPerson(@RequestBody PersonDTO personDTO) {
+        personService.addNewPerson(personDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(HttpStatus.CREATED);
+    }
+}
