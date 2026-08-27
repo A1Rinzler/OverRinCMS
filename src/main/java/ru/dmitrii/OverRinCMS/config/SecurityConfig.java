789fd0810b14abeed36fcfeb3bbd325802a60d53
Dmitrii Frolov <tryfrod@gmail.com>
2026-08-27 18:56:49 +0300

    Добавлен AuthenticationProvider и AuthenticationManager для работы с авторизацией. AuthenticationProvider дефолтный сам проверяет логин и пароль. в securityFilterChain добавил правила, куда кто может заходить. запрашивать ли сессию и не предоставлять стандартную форму логина от springsecurity

--- src/main/java/ru/dmitrii/OverRinCMS/config/SecurityConfig.java
+++ src/main/java/ru/dmitrii/OverRinCMS/config/SecurityConfig.java
@@ -1,22 +1,55 @@
 package ru.dmitrii.OverRinCMS.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.dmitrii.OverRinCMS.service.PersonDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final PersonDetailService personDetailService;
+
+    @Autowired
+    public SecurityConfig(PersonDetailService personDetailService) {
+        this.personDetailService = personDetailService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(personDetailService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf-> csrf.disable())
-            .authorizeHttpRequests(authorizeRequests -> authorizeRequests.anyRequest().permitAll());
+                .authorizeHttpRequests(authorizeRequests ->authorizeRequests
+                        .requestMatchers("/admin/auth/login").permitAll()
+                        .requestMatchers("/admin","/admin/**").hasRole("USER")
+                        .requestMatchers("/news","/files","/rules","/about","/lk","/error").permitAll()
+                        .anyRequest().permitAll())
+        .formLogin(formLogin -> formLogin.disable()
+        .sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+        );
         return http.build();
     }
 
