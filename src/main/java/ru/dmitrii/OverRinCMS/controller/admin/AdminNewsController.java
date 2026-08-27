884fc2ab89f8c9e9f4bbfdc17bbaa601ab088664
Dmitrii Frolov <tryfrod@gmail.com>
2026-08-27 18:50:01 +0300

    рефакторинг

--- src/main/java/ru/dmitrii/OverRinCMS/controller/admin/AdminNewsController.java
+++ src/main/java/ru/dmitrii/OverRinCMS/controller/admin/AdminNewsController.java
@@ -1,4 +1,4 @@
-package ru.dmitrii.OverRinCMS.controller;
+package ru.dmitrii.OverRinCMS.controller.admin;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
